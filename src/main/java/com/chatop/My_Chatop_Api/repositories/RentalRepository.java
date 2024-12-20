dd9207cad97f06aae15c9753b59743e3f865d311
coraliehaller <haller.coralie.15@gmail.com>
2024-12-20 20:59:54 +0100

    Cleaning du RentalRepo

--- src/main/java/com/chatop/My_Chatop_Api/repositories/RentalRepository.java
+++ src/main/java/com/chatop/My_Chatop_Api/repositories/RentalRepository.java
@@ -8,10 +8,4 @@ import java.util.List;
 @Repository
 public interface RentalRepository extends JpaRepository<Rental, Long> {
 
-    // MTHD PR TROUVER UN RENTAL À PARTIR DE L'ID DU PROPRIETAIRE
-    // Notation pointée (Owner.Id) pr naviguer dans la relation @ManyToOne et accéder à l'attribut id de l'entité User
-    //List<Rental> findByOwner_Id(Long ownerId);
-
-    // MTHD PR TROUVER UN RENTAL À PARTIR DU NOM DU RENTAL
-   // List<Rental> findByNameContaining(String name);
 }
